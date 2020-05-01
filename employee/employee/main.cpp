d92f74a2a8a44e40eba6ab5f51ae81fae324eee0
changdeng <353850613@qq.com>
2020-05-01 11:06:30 +0800

    Start

---
+++ employee/employee/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  employee
+//
+//  Created by Chang Deng on 2020/5/1.
+//  Copyright © 2020 Chang Deng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
