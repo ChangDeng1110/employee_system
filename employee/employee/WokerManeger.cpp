2e641646f855d9dca12e988775ae9f4ae79cfe7d
changdeng <353850613@qq.com>
2020-05-01 11:21:46 +0800

    test1

---
+++ employee/employee/WokerManeger.cpp
@@ -0,0 +1,10 @@
+//
+//  WokerManeger.cpp
+//  employee
+//
+//  Created by Chang Deng on 2020/5/1.
+//  Copyright © 2020 Chang Deng. All rights reserved.
+//
+
+#include "WokerManeger.hpp"
+
---
+++ employee/employee/WokerManeger.hpp
@@ -0,0 +1,26 @@
+//
+//  WokerManeger.hpp
+//  employee
+//
+//  Created by Chang Deng on 2020/5/1.
+//  Copyright © 2020 Chang Deng. All rights reserved.
+//
+
+#ifndef WokerManeger_hpp
+#define WokerManeger_hpp
+
+#include <stdio.h>
+#include <iostream>
+
+
+using namespace std;
+
+class WorkerManeger
+{
+public:
+    WorkerManeger();
+    ~WokerManeger();
+}
+
+
+#endif /* WokerManeger_hpp */
